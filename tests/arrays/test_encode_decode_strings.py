8375fb7ff3012eb6fea283eaae0f9c854be2c007
charan-kilana <charansurya2020@gmail.com>
2026-05-03 20:40:00 -0500

    added tests for encode-decode

---
+++ tests/arrays/test_encode_decode_strings.py
@@ -0,0 +1,28 @@
+from arrays.encode_decode_strings import Solution
+
+sol = Solution()
+
+def test_basic_strings():
+    strs = ["neet", "code", "love"]
+    encoded = sol.encode(strs)
+    assert sol.decode(encoded) == strs
+
+def test_single_string():
+    strs = ["hello"]
+    encoded = sol.encode(strs)
+    assert sol.decode(encoded) == strs
+
+def test_empty_list():
+    strs = []
+    encoded = sol.encode(strs)
+    assert sol.decode(encoded) == strs
+
+def test_empty_string():
+    strs = [""]
+    encoded = sol.encode(strs)
+    assert sol.decode(encoded) == strs
+
+def test_multiple_strings():
+    strs = ["act", "cat", "dog"]
+    encoded = sol.encode(strs)
+    assert sol.decode(encoded) == strs
