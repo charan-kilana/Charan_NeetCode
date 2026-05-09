63a16cedb81f4fdec7d947de00a7e8c05c586a69
charan-kilana <charansurya2020@gmail.com>
2026-05-08 23:25:08 -0500

    ater fix

---
+++ tests/two_pointers/test_two_sum_ii.py
@@ -0,0 +1,18 @@
+from two_pointers.two_sum_ii import Solution
+
+sol = Solution()
+
+def test_basic():
+    assert sol.twoSum([2, 7, 11, 15], 9) == [1, 2]
+
+def test_middle_pair():
+    assert sol.twoSum([1, 2, 3, 4, 6], 7) == [3, 4]
+
+def test_last_pair():
+    assert sol.twoSum([1, 2, 3, 4, 5], 9) == [4, 5]
+
+def test_negative_numbers():
+    assert sol.twoSum([-3, -1, 0, 2, 4], 1) == [1, 5]
+
+def test_mixed_numbers():
+    assert sol.twoSum([-2, 0, 3, 7], 5) == [1, 4]
--- two_pointers/two_sum_ii.py
+++ two_pointers/two_sum_ii.py
@@ -1,12 +1,9 @@
+from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # BFM
-        output = []
         for i in range(len(numbers)):
             for j in range(i+1, len(numbers)):
                 if numbers[i] != numbers[j]:
                     if numbers[i] + numbers[j] == target:
-                        output.append(i+1)
-                        output.append(j+1)
-        return output
+                        return [i+1,j+1]
 
