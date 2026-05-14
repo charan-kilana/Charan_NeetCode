eb79813a00780dd72b2f0d1aa733450e54d6b6ca
charan-kilana <charansurya2020@gmail.com>
2026-05-13 20:51:25 -0500

    added test code

---
+++ tests/two_pointers/test_longest_repeating_character_replacement.py
@@ -0,0 +1,24 @@
+from sliding_window.longest_repeating_character_replacement import Solution
+
+sol = Solution()
+
+def test_basic():
+    assert sol.characterReplacement("AAABABB", 1) == 5
+
+def test_example_two():
+    assert sol.characterReplacement("AABBBABBA", 1) == 5
+
+def test_all_same():
+    assert sol.characterReplacement("AAAA", 2) == 4
+
+def test_k_zero():
+    assert sol.characterReplacement("AABABBA", 0) == 2
+
+def test_replace_two():
+    assert sol.characterReplacement("ABAB", 2) == 4
+
+def test_single_character():
+    assert sol.characterReplacement("A", 1) == 1
+
+def test_no_replacement_needed():
+    assert sol.characterReplacement("BBBBB", 0) == 5
